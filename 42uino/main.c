59dfff91d2abd21ce5e194f41d46d37e5dcb8355
ItsHade <malo.burnet@gmail.com>
2025-11-25 23:24:56 +0100

    Arduino ide blink working, raW AVR C flashing but not blinking led

--- 42uino/main.c
+++ 42uino/main.c
@@ -2,11 +2,19 @@
 #include <util/delay.h>
 
 int main(void) {
-    DDRB |= (1 << PB5);  // LED on pin 13
+    /* DDRB |= (1 << PB3);  // Digital pin 13 (D13) --> builtin LED */
+    DDRH |= (1 << PH6);
+    /* DDRB |= (1 << PB1);  // Digital pin 9 (D9) */ 
 
     while (1) {
-        PORTB ^= (1 << PB5);
-        _delay_ms(500);
+        /* PORTB |= (1 << PB3); */
+        PORTH |= (1 << PH6);
+        /* PORTB |= (1 << PB1); */
+        _delay_ms(100);
+        /* PORTB &= ~(1 << PB3); */
+        PORTH &= ~(1 << PH6);
+        /* PORTB &= ~(1 << PB1); */
+        _delay_ms(100);
     }
 }
 
