ae677f0473ca622882946263baebee140f5eae2d
ItsHade <malo.burnet@gmail.com>
2025-11-29 16:14:37 +0100

    can be used to flash atmega328p board we soldered at school

--- 42uino/main.c
+++ 42uino/main.c
@@ -2,18 +2,12 @@
 #include <util/delay.h>
 
 int main(void) {
-    /* DDRB |= (1 << PB3);  // Digital pin 13 (D13) --> builtin LED */
-    DDRH |= (1 << PH6);
-    /* DDRB |= (1 << PB1);  // Digital pin 9 (D9) */ 
+    DDRB |= (1 << PB5);  // Digital pin 13 (D13) --> builtin LED */
+    DDRB |= (1 << PB1);  // Digital pin 9 (D9) */ 
 
     while (1) {
-        /* PORTB |= (1 << PB3); */
-        PORTH |= (1 << PH6);
-        /* PORTB |= (1 << PB1); */
-        _delay_ms(100);
-        /* PORTB &= ~(1 << PB3); */
-        PORTH &= ~(1 << PH6);
-        /* PORTB &= ~(1 << PB1); */
+        PORTB ^= (1 << PB5);
+        PORTB ^= (1 << PB1);
         _delay_ms(100);
     }
 }
